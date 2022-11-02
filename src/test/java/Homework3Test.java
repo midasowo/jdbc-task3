cc6e77dc389c4452805794b47d9121ea0d46b2af
midsowo <kmiduch@gmail.com>
2022-11-02 22:32:36 +0100

    created test

--- src/test/java/Homework3Test.java
+++ src/test/java/Homework3Test.java
@@ -1,10 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+
+
 public class Homework3Test {
 
     @Test
-    void shouldReturnActorsByBerFragment() {
+    void shouldReturnThreeBooksRentedButNotReturned() {
+        //WHEN
+        final var books = Homework3.getRentedAndNotReturnedBooks
+                (ApplicationPropertiesProvider.getApplicationProperties().getProperty("jdbc.url"));
 
+        //THEN
+        assertThat(books).extracting(Books::getName, Books::getAuthor, Books::getPublisher).containsExactly(
+                tuple("Wojna Makowa", "Rebecca F. Kuang", "Fabryka Slow"),
+                tuple("Upadek Gondolinu", "J.R.R. Tolkien", "Proszynski Media"),
+                tuple("Xiao Long. Bialy Tygrys", "Dawid Juraszek", "Fabryka Slow")
+        );
     }
 
 }
